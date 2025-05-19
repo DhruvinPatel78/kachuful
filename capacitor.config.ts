0c4b8b96ae54a7a1e197f734afd38a7eebbbabd2
DhruvinPatel78 <patel.dhruvinpatel@gmail.com>
2025-05-19 16:03:07 +0530

    android

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.kachuful.com',
+  appName: 'Kachuful',
+  webDir: 'dist',
+  backgroundColor: '#0f172acc'
+};
+
+export default config;
