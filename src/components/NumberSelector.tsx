42e7ba3f9d6bfa74429c3ff408c30c32acd5ce21
Dhruvin Patel <patel.dhruvinpatel@gmail.com>
2025-08-04 15:32:40 +0530

    Updated NumberSelector.tsx

--- src/components/NumberSelector.tsx
+++ src/components/NumberSelector.tsx
@@ -50,7 +50,7 @@ const NumberSelector: React.FC<NumberSelectorProps> = ({
         </div>
         
         <Button 
-          variant="danger" 
+          variant="outline" 
           size="sm" 
           onClick={handleIncrement}
           disabled={value >= maxNumber}
